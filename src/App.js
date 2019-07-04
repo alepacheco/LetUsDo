423dad823661da14fc512cc61672fd6d4c9920ed
Alejandro Pacheco <alepacheco99@gmail.com>
2019-07-04 21:30:03 +0100

    Added basic home page

--- src/App.js
+++ src/App.js
@@ -18,13 +18,6 @@ class App extends React.Component {
     const activeStyle = { color: 'blue' };
     return (
       <div>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/fuel-savings" component={FuelSavingsPage} />
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>React Slingshot</h1>
-
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
+    <div className="home-page center-v-h">
+      <h1 className="main-text">We Do</h1>
+      <p className="sub-text">Lorem isupm </p>
+      <Card>
+        <Form className="cccc">
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>What task do you need done?</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+        </Form>
+        <Button className="get-it-done-button">Get it done</Button>
+      </Card>
     </div>
   );
 };
