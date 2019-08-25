c239ff7d751010f144b19358cbe8a1d6f3c50f95
Alejandro Pacheco <alepacheco99@gmail.com>
2019-08-25 23:21:32 +0100

    added icons to feature list

--- src/components/FeaturesList.js
+++ src/components/FeaturesList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/components/FeaturesList.scss';
+import cafe from '../static/images/cafe.png'
+import london from '../static/images/london-bridge.png'
+import cash from '../static/images/cash.png'
 
-
-const ListElement = (props) => {
-
+const ListElement = ({image}) => {
   return (<div className="item">
+    <img className="feature-icon" src={image}></img>
     <div className="list-element-title">Beautiful themes that are responsive and customizable</div>
     <div className="list-element-body">No design skills needed. You have complete control over the look and feel of your website, from its layout, to content</div>
   </div>);
 };
-
 
 export class FeaturesList extends React.Component {
   constructor(props) {
@@ -20,9 +21,9 @@ export class FeaturesList extends React.Component {
     return (
       <div className="features-list">
         <div className="container">
-          <ListElement />
-          <ListElement />
-          <ListElement />
+        <ListElement image={london}/>
+        <ListElement image={cash}/>
+        <ListElement image={cafe}/>
         </div>
       </div>
     );
