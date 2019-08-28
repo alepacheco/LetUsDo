3acd851875a143d739f7f69f3730c45e5989408b
Alejandro Pacheco <alepacheco99@gmail.com>
2019-08-28 22:13:34 +0100

    Excude coverage

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,3 @@
+/* istanbul ignore file */
 export const SAVE_FUEL_SAVINGS = 'SAVE_FUEL_SAVINGS';
 export const CALCULATE_FUEL_SAVINGS = 'CALCULATE_FUEL_SAVINGS';
--- src/types/index.js
+++ src/types/index.js
@@ -1,24 +1,3 @@
+/* istanbul ignore file */
 // Centralized propType definitions
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  shape, number, bool, string, oneOfType,
-} from 'prop-types';
-
-export const savings = shape({
-  monthly: oneOfType([number, string]),
-  annual: oneOfType([number, string]),
-  threeYear: oneOfType([number, string]),
-});
-
-export const fuelSavings = shape({
-  newMpg: oneOfType([number, string]),
-  tradeMpg: oneOfType([number, string]),
-  newPpg: oneOfType([number, string]),
-  tradePpg: oneOfType([number, string]),
-  milesDriven: oneOfType([number, string]),
-  milesDrivenTimeframe: string,
-  displayResult: bool,
-  dateModified: string,
-  necessaryDataIsProvidedToCalculateSavings: bool,
-  savings,
-});
