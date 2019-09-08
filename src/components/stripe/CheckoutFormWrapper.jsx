6dc6e2b35e153b8c79ea5d70e4942300cc994211
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-08 16:45:24 +0100

    add now env

--- src/components/stripe/CheckoutFormWrapper.jsx
+++ src/components/stripe/CheckoutFormWrapper.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
 
+require('now-env');
+
 const CheckoutFormWrapper = () => (
   <StripeProvider apiKey={process.env.STRIPE_FRONT}>
     <Elements>
