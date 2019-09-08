5d75bccab1df1600eb420ea427679480f3c2c685
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-08 14:39:24 +0100

    Lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 /* eslint-disable import/no-named-as-default */
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import PropTypes from "prop-types";
-import React from "react";
-import { hot } from "react-hot-loader";
+import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import HomePage from './pages/HomePage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  children: PropTypes.element
-};
+const App = () => (
+  <div>
+    <Switch>
+      <Route path="/" component={HomePage} />
+    </Switch>
+  </div>
+);
 
 export default hot(module)(App);
