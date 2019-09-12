53eb13238f163eac63f273283dbedae1dc103ca6
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-12 20:34:16 +0100

    Adding apple pay

--- src/components/CheckoutPopUp/stripe/CheckoutForm.tsx
+++ src/components/CheckoutPopUp/stripe/CheckoutForm.tsx
@@ -24,21 +24,8 @@ const tryApplePay = async ({ stripe }: { stripe: ReactStripeElements.StripeProps
   });
 
   const canUsePayBotton = await paymentRequest.canMakePayment();
-  if (canUsePayBotton) {
-    // @ts-ignore
-    const elements = stripe.elements();
 
-    const prButton = elements.create('paymentRequestButton', {
-      paymentRequest
-    });
-
-    prButton.mount('#checkout-bottom');
-
-    const element = document.getElementById('checkout-form');
-    if (element) {
-      element.style.display = 'none';
-    }
-  } else {
+  if (!canUsePayBotton) {
     const element = document.getElementById('checkout-bottom');
     if (element) {
       element.style.display = 'none';
@@ -70,15 +57,11 @@ const tryApplePay = async ({ stripe }: { stripe: ReactStripeElements.StripeProps
     );
 
     if (confirmError || !process.env.STRIPE_FRONT) {
-      // Report to the browser that the payment failed, prompting it to
       // re-show the payment interface, or show an error message and close
-      // the payment interface.
       console.log('apple pay failed');
       ev.complete('fail');
     } else {
-      // Report to the browser that the confirmation was successful, prompting
-      // it to close the browser payment method collection interface.
-      console.log('apple pay collected');
+      console.log('apple pay completed (not payed)');
       ev.complete('success');
       // Let Stripe.js handle the rest of the payment flow.
       const { error } = await stripe.handleCardPayment(process.env.STRIPE_FRONT);
@@ -87,7 +70,6 @@ const tryApplePay = async ({ stripe }: { stripe: ReactStripeElements.StripeProps
         // The payment failed -- ask your customer for a new payment method.
       } else {
         console.log('apple pay payed');
-        // The payment has succeeded.
       }
     }
   });
@@ -101,7 +83,7 @@ export const CheckoutForm: React.FC<{
   actions: { setDialog: (state: string) => void };
 }> = ({ email, stripe, taskText, actions: { setDialog }, validEmail }) => {
   if (stripe) {
-    // tryApplePay({ stripe });
+    tryApplePay({ stripe });
   }
 
   const onClick = async () => {
