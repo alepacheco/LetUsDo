c15786f7849d8fa6085780f47c6b25ea688b1ab0
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-17 13:54:02 +0100

    Adding asssets and scripts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const withSass = require('@zeit/next-sass')
+require('dotenv').config()
+
+const Dotenv = require('dotenv-webpack')
 
 function HACK_removeMinimizeOptionFromCssLoaders(config) {
   config.module.rules.forEach(rule => {
@@ -16,9 +19,21 @@ function HACK_removeMinimizeOptionFromCssLoaders(config) {
 module.exports = withSass({
   webpack(config) {
     HACK_removeMinimizeOptionFromCssLoaders(config);
-    config.resolve.alias['components'] = path.join(__dirname, 'src/components')
-    config.resolve.alias['pages'] = path.join(__dirname, 'pages')
-    config.resolve.alias['src'] = path.join(__dirname, 'src')
+    config.resolve.alias['components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['pages'] = path.join(__dirname, 'pages');
+    config.resolve.alias['src'] = path.join(__dirname, 'src');
+
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
     return config;
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import App from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
+import Head from 'next/head'
+import 'src/styles/styles.scss';
+
 import configureStore from 'src/store/configureStore';
 import initialState from 'src/store/initialState';
 import initGa from 'src/utils/analytics';
@@ -14,9 +17,26 @@ class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
         return (
-            <Provider store={store}>
-                <Component {...pageProps} />
-            </Provider>
+            <>
+                <Head>
+                    <link rel="preconnect" href="https://m.stripe.com" />
+                    <script async src="https://js.stripe.com/v3/"></script>
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                        crossOrigin="anonymous"
+                    />
+                    <link rel="icon" href="/static/favicon.ico" type="image/x-icon"/>
+
+                    <link rel="shortcut icon" type="image/png" href="/static/favicon.ico"/>
+                    <title>Let Us Do</title>
+                </Head>
+                <Provider store={store}>
+                    <Component {...pageProps} />
+                </Provider>
+            </>
+
         )
     }
 }
