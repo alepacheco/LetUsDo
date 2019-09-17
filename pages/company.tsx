16f9734ce232106057e1e3c74140a2d48b89b534
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-18 00:20:35 +0100

    Add demo pages

---
+++ pages/company.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CompanyPage = () => <>TODO</>;
+
+export default CompanyPage;
---
+++ pages/docs.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PublicApiPage = () => <>Public api: TODO</>;
+
+export default PublicApiPage;
