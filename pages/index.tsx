39c46fafa98f377a71708c285183f1d71e588666
Alejandro Pacheco <alepacheco99@gmail.com>
2019-09-18 13:32:03 +0100

    Add live chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,31 @@ const CheckoutPopUpByType = dynamic(() =>
 );
 const FeaturesList = dynamic(() => import('src/components/FeaturesList'));
 const TaskBox = dynamic(() => import('src/components/TaskBox'));
+import Head from 'next/head'
 
-const HomePage = () => (
-  <>
-    <LandingHeader />
-    <TaskBox />
-    <FullFeatureList />
-    <FeaturesList />
-    <Guidelines />
-    <Footer />
-    <CheckoutPopUpByType />
-  </>
-);
+class HomePage extends React.Component {
+  componentDidMount() {
+    // @ts-ignore
+    window.$crisp = [];
+    // @ts-ignore
+    window.CRISP_WEBSITE_ID = "1235c6ea-ec25-4935-8480-4517d1b2c930";
+  }
+
+  render() {
+    return (
+      <>
+        <LandingHeader />
+        <TaskBox />
+        <FullFeatureList />
+        <FeaturesList />
+        <Guidelines />
+        <Footer />
+        <CheckoutPopUpByType />
+        <Head>
+          <script async defer src="https://client.crisp.chat/l.js"/>
+        </Head>
+      </>);
+  }
+}
 
 export default HomePage;
